ac7f754f7d1eb45e52839fe098bbcabbc82c34b3
admin <vinod.s@renewin.com>
2021-05-18 11:46:30 +0530

    Peepalgraphcommit1

---
+++ auth/AuthConfig.ts
@@ -0,0 +1,11 @@
+export const AuthConfig = {
+  appId: '82acaa60-24e2-4ed7-a39b-21ac1dc399fe',
+  appScopes: [
+    'openid',
+    'offline_access',
+    'profile',
+    'User.Read',
+    'MailboxSettings.Read',
+    'Calendars.ReadWrite'
+  ]
+};
